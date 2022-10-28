78a6c4902b074d14894cbaf0d17bd8e38bc959f6
Priyam Tejaswin <priyamtejaswin@gmail.com>
2022-10-28 17:31:47 -0400

    Added tests once the app starts. Needs to be converted to JUnit tests later.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -14,6 +14,9 @@ import android.widget.TextView;
 
 import com.example.helloworld.ml.QaClient;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,8 +40,11 @@ public class MainActivity extends AppCompatActivity {
     public void updateText(View view) {
         Editable query = inputText.getText();
         int[][] bertids = qaClient.q2ids(query.toString());
-        answerView.setText(bertids[0].toString());
-        System.out.println(bertids[0]);
+        for (int element : bertids[0]) {
+            System.out.println(element);
+        }
+        System.out.println("Done!");
+        answerView.setText("Processed! " + Arrays.toString(bertids[0]));
         System.out.println("Clicked.");
     }
 
@@ -49,6 +55,42 @@ public class MainActivity extends AppCompatActivity {
 //        handler.post(() -> qaClient.loadDictionary());
         Log.v(TAG, "Calling loadDictionary");
         qaClient.loadDictionary();
+
+        // Run all tests here for parity!
+        String[] texts = {"is this pizza vegetarian?",
+                "Is this pizza vegetarian?",
+                "how many dogs are there in this picture ?",
+                "How many Dogs are there in this picture ?",
+                "what is next to the number 102"
+        };
+        int[][] truths = {{101, 2003, 2023, 10733, 23566, 1029, 102},
+                {101, 2003, 2023, 10733, 23566, 1029, 102},
+                {101, 2129, 2116, 6077, 2024, 2045, 1999, 2023, 3861, 1029, 102},
+                {101, 2129, 2116, 6077, 2024, 2045, 1999, 2023, 3861, 1029, 102},
+                {101, 2054, 2003, 2279, 2000, 1996, 2193, 9402, 102}
+        };
+
+        for (int i = 0; i < texts.length; i++) {
+            String t = texts[i];
+            List<Integer> pred = new ArrayList<Integer>();
+            int[] target = truths[i];
+
+            for (Integer e : qaClient.q2ids(t)[0]) {
+                if (e != 0) {
+                    pred.add(e);
+                    if (e == 102) break;
+                } else break;
+            }
+
+            int[] ids = new int[0];
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                ids = pred.stream().mapToInt(Integer::intValue).toArray();
+            }
+
+            System.out.println("p: " + Arrays.toString(ids));
+            System.out.println("t: " + Arrays.toString(target));
+            assert Arrays.equals(ids, target) : "LAST ONE FAILED!";
+        }
     }
 
     @Override
